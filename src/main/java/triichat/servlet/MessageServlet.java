d588e7065e144012fcd7b738d7ffba37126477e1
Margret Tumbokon <margrettumbokon@utexas.edu>
2016-04-05 19:31:27 -0500

    added MessageServlet doPost code

--- src/main/java/triichat/servlet/MessageServlet.java
+++ src/main/java/triichat/servlet/MessageServlet.java
@@ -1,15 +1,22 @@
 package triichat.servlet;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import triichat.Message;
 import triichat.OfyService;
+import triichat.Trii;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by anoop on 3/30/16.
@@ -37,9 +44,31 @@ public class MessageServlet extends HttpServlet {
 
         String messageBody = request.getParameter("body");
         Long triiID = Long.parseLong(request.getParameter("trii_id"));
+        UserService userService = UserServiceFactory.getUserService();
+        User gUser = userService.getCurrentUser();
+        triichat.User user = triichat.User.findUser(gUser);
+        if(user == null){
+            user = triichat.User.createUser(gUser);
+        }
+        Trii trii = OfyService.getTrii(triiID);
+        // get most recent message to make it the parent as default
+        // TODO: eventually make this use actual parent
+        Set<Message> parents = new HashSet<Message>();
+        parents.add(getMostRecent(trii));
+        // Create message
+        Message.createMessage(messageBody, parents, user);
 
-        // TODO: insert this message into the database
         // TODO: notify any users who are listening to this trii
         System.out.println("trii: " + triiID + ", message: " + messageBody);
+    }
+
+    private Message getMostRecent(Trii trii){
+        Message retval = trii.getRoot();
+
+        while(!retval.getReplies().isEmpty()){
+            retval = retval.getReplies().iterator().next();
+        }
+
+        return retval;
     }
 }
