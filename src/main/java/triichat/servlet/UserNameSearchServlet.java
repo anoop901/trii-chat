1dc6667c35da470dbbd87adb33642af0f93a002f
Margret Tumbokon <margrettumbokon@utexas.edu>
2016-04-05 19:54:53 -0500

    added UserNameSearchServlet which searches for users by name

---
+++ src/main/java/triichat/servlet/UserNameSearchServlet.java
@@ -0,0 +1,46 @@
+package triichat.servlet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import triichat.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Margret on 4/5/2016.
+ */
+public class UserNameSearchServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //write this
+        String name = request.getParameter("name");
+
+        JSONObject found = new JSONObject();
+        JSONArray foundUsers = new JSONArray();
+
+        if(name != null){
+            List<User> users = User.findUserByName(name);
+            for(User u : users){
+                foundUsers.put(u.getId());
+            }
+        }
+
+        try {
+            found.put("users", foundUsers);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        response.getWriter().println(found);
+    }
+}
