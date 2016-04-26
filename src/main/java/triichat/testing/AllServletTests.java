f6232e32d6d8eeec2752f5065d60a23bfa0b65ad
Margret Tumbokon <margrettumbokon@utexas.edu>
2016-04-26 15:25:07 -0500

    Added Group Post test in GroupServletTest. Also added GroupServletTest to AllServletTests

--- src/main/java/triichat/testing/AllServletTests.java
+++ src/main/java/triichat/testing/AllServletTests.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         triichat.testing.servlet.ExampleServletTest.class,
-        triichat.testing.servlet.MeServletTest.class
+        triichat.testing.servlet.MeServletTest.class,
+        triichat.testing.servlet.GroupServletTest.class
 })
 /**
  * Created by Margret on 4/25/2016.
