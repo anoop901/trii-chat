57962cfa29c31d828d6a285ed4554926c47028fe
Margret Tumbokon <margrettumbokon@utexas.edu>
2016-05-02 12:47:42 -0500

    Finished up servlet tests

---
+++ src/main/java/triichat/testing/servlet/Constants.java
@@ -0,0 +1,19 @@
+package triichat.testing.servlet;
+
+/**
+ * Created by Margret on 5/2/2016.
+ */
+public class Constants {
+
+    public static final String LOCALHOST = "http://localhost:4567";
+    public static final String TRII_SERVLET_MAPPING = "/trii";
+    public static final String MESSAGE_SERVLET_MAPPING = "/message";
+
+    public static final int TEST_MAX = 25;
+
+    public static final String WEBXML_LOC = "src/main/webapp/WEB-INF/web.xml";
+    public static final String ENV_EMAIL = "bozo1@clown.com";
+    public static final String ENV_USER_ID = "bozo1";
+    public static final String ENV_AUTH_DOMAIN = "clown1.com";
+    public static final boolean ENV_IS_ADMIN = false;
+}
